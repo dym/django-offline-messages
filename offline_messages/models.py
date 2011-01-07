4004dd74f5c6bb48399a1b474abf7421ceea0a9d
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2011-01-07 16:58:24 +0200

    Added message level.

--- offline_messages/models.py
+++ offline_messages/models.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8; mode: python; -*-
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.encoding import force_unicode
+from django.contrib.messages import constants
 
 class OfflineMessage(models.Model):
     user = models.ForeignKey(User)
+    level = models.IntegerField(default=constants.INFO)
     message = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return u"%s" % self.message
+        return force_unicode(self.message)
