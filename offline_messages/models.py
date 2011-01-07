b00a90c9fbfdf05c4fa0e9b70f4e8e416646a740
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2011-01-07 17:58:24 +0200

    Added tags to messages, so now css class is added for every message based on level and tag.

--- offline_messages/models.py
+++ offline_messages/models.py
@@ -7,6 +7,7 @@ from django.contrib.messages import constants
 class OfflineMessage(models.Model):
     user = models.ForeignKey(User)
     level = models.IntegerField(default=constants.INFO)
+    tags = models.CharField(max_length=100, null=True, blank=True)
     message = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
--- offline_messages/utils.py
+++ offline_messages/utils.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8; mode: python -*-
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.utils.encoding import force_unicode
+from django.contrib.messages.utils import get_level_tags
 
 from offline_messages.models import OfflineMessage
 
 
-def create_offline_message(user, message):
+def create_offline_message(user, message, level=constants.INFO):
     if not isinstance(user, User):
         user = User.objects.get(username=user)
 
-    OfflineMessage.objects.create(user=user, message=message)
+    level_tags = get_level_tags()
+    label_tag = force_unicode(level_tags.get(level, ''), strings_only=True)
+
+    OfflineMessage.objects.create(user=user,
+                                  level=level,
+                                  tags=label_tag,
+                                  message=message)
