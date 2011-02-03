9f74d10608f4370f5d5c80258d4e657fc809aba5
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2011-02-03 15:43:40 +0200

    Subsituted tags field with tags property.

--- offline_messages/models.py
+++ offline_messages/models.py
@@ -3,13 +3,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.encoding import force_unicode
 from django.contrib.messages import constants
+from django.contrib.messages.utils import get_level_tags
 
 class OfflineMessage(models.Model):
     user = models.ForeignKey(User)
     level = models.IntegerField(default=constants.INFO)
-    tags = models.CharField(max_length=100, null=True, blank=True)
     message = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return force_unicode(self.message)
+
+    @property
+    def tags(self):
+        level_tags = get_level_tags()
+        return force_unicode(level_tags.get(self.level, ''), strings_only=True)
