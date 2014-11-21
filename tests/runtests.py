52a26ef3a251020380f05b5807887d810764f7aa
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2014-11-21 17:13:21 +0200

    Update travis.yml and runtests.py

--- tests/runtests.py
+++ tests/runtests.py
@@ -10,7 +10,10 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 try:
     from django_coverage.coverage_runner import CoverageRunner as TestRunner
 except ImportError:
-    from django.test.simple import DjangoTestSuiteRunner as TestRunner
+    try:
+        from django.test.runner import DiscoverRunner as TestRunner
+    except ImportError:
+        from django.test.simple import DjangoTestSuiteRunner as TestRunner
 
 def runtests():
     test_runner = TestRunner(verbosity=1, interactive=True)
